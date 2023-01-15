f36f031bbae78c371ba309b42e8dc13c47d57c8f
kutuzoffmoscow <kutuzoffmoscow@gmail.com>
2023-01-15 23:31:37 +0300

    добавил код

--- lesson_1/exp2/Program.cs
+++ lesson_1/exp2/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 15;
+int b = 30;
+int c = 2;
+
+
+Console.WriteLine("Числа: " + a + ", " + b + ", " + c);
+
+if (a > b)
+{
+    if(a > c)
+    {
+        Console.WriteLine(a);
+    }
+    else
+    {
+        Console.WriteLine(c);
+    }
+}
+else
+{
+    if(b > c)
+    {
+        Console.WriteLine(b);
+    }
+    else
+    {
+        Console.WriteLine(c);
+    }
+}
